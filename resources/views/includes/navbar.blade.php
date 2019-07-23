dc0f80357bb58207042902ef39395ab6a58095ec
Handika <handika@aiia.co.id>
2019-07-23 07:59:52 +0700

    Initial commit

---
+++ resources/views/includes/navbar.blade.php
@@ -0,0 +1,70 @@
+<nav class="bottom-navbar">
+    <div class="container container-navbar">
+        <ul class="nav page-navigation">
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('home') }}">
+                    <i class="mdi mdi-view-dashboard menu-icon"></i>
+                    <span class="menu-title">Dashboard</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <i class="mdi mdi-newspaper menu-icon"></i>
+                    <span class="menu-title">Job Vacancy</span>
+                    <i class="menu-arrow"></i>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <i class="mdi mdi-human-greeting menu-icon"></i>
+                    <span class="menu-title">Job Seekers</span>
+                    <i class="menu-arrow"></i>
+                </a>
+                <div class="submenu">
+                    <ul>
+                        <li class="nav-item"><a class="nav-link" href="pages/ui-features/buttons.html">White List</a></li>
+                        <li class="nav-item"><a class="nav-link" href="pages/ui-features/typography.html">Black List</a></li>
+                    </ul>
+                </div>
+            </li>
+            <li class="nav-item">
+                <a href="pages/icons/mdi.html" class="nav-link">
+                    <i class="mdi mdi-file-document menu-icon"></i>
+                    <span class="menu-title">Job Applications</span>
+                    <i class="menu-arrow"></i>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <i class="mdi mdi-database menu-icon"></i>
+                    <span class="menu-title">Master Data</span>
+                    <i class="menu-arrow"></i>
+                </a>
+                <div class="submenu">
+                    <ul>
+                        <li class="nav-item"><a class="nav-link" href="pages/ui-features/buttons.html">Recruitment Stage</a></li>
+                        <li class="nav-item"><a class="nav-link" href="pages/ui-features/typography.html">Degree</a></li>
+                        <li class="nav-item"><a class="nav-link" href="pages/ui-features/typography.html">Provinces</a></li>
+                        <li class="nav-item"><a class="nav-link" href="pages/ui-features/typography.html">District</a></li>
+                        <li class="nav-item"><a class="nav-link" href="pages/ui-features/typography.html">Sub-District</a></li>
+                        <li class="nav-item"><a class="nav-link" href="pages/ui-features/typography.html">Villages</a></li>
+                    </ul>
+                </div>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <i class="mdi mdi-human-male-female menu-icon"></i>
+                    <span class="menu-title">User Management</span>
+                    <i class="menu-arrow"></i>
+                </a>
+                <div class="submenu">
+                    <ul>
+                        <li class="nav-item"><a class="nav-link" href="{{ route('users.index') }}">List</a></li>
+                        <li class="nav-item"><a class="nav-link" href="pages/ui-features/typography.html">Permissions</a></li>
+                        <li class="nav-item"><a class="nav-link" href="pages/ui-features/typography.html">Roles</a></li>
+                    </ul>
+                </div>
+            </li>
+        </ul>
+    </div>
+</nav>
